08d7885fda2c84fd248541e429a6556182d2c7f0
Abdullah <aabdullahsmx2014@gmail.com>
2021-11-16 17:40:37 +0100

    First commit

---
+++ src/edu/epsevg/prop/lab/c4/ConnectaBroIo.java
@@ -0,0 +1,37 @@
+package edu.epsevg.prop.lab.c4;
+
+/**
+ * Jugador aleatori
+ * "Alea jacta est"
+ * @author Profe
+ */
+public class ConnectaBroIo implements Jugador, IAuto {
+    private String nom;
+    
+    public ConnectaBroIo(){
+        nom = "ConnectaBroIo";
+    }
+  
+    public int moviment(Tauler t, int color){
+        int col = 0;
+        for(int i=0; i < t.getMida(); i++){
+            if(t.movpossible(i)){
+                Tauler aux = new Tauler(t);
+                if(aux.solucio(i, color)){
+                    return i;
+                } else {
+                    // Llamar a minimax, donde yo maximizo y el otro minimza
+                    col = 1;
+                }
+
+            }
+        }
+        return col;
+    }
+  
+    public String nom(){
+        return nom;
+    }
+}
+
+
