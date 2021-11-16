e8d341ad84e0ba21979f6f30814f2ec3d52776e5
Abdullah <aabdullahsmx2014@gmail.com>
2021-11-16 19:32:25 +0100

    Start moviment function to determine heuristic

--- src/edu/epsevg/prop/lab/c4/ConnectaBroIo.java
+++ src/edu/epsevg/prop/lab/c4/ConnectaBroIo.java
@@ -7,32 +7,38 @@ package edu.epsevg.prop.lab.c4;
  */
 public class ConnectaBroIo implements Jugador, IAuto {
     private String nom;
+    private int profundidad;
     
-    public ConnectaBroIo(){
+    public ConnectaBroIo(int profundidad){
         nom = "ConnectaBroIo";
-        
+        this.profundidad = profundidad;
+    }
+
+    public String nom(){
+        return nom;
     }
   
     public int moviment(Tauler t, int color){
         int col = 0;
-        for(int i=0; i < t.getMida(); i++){
-            if(t.movpossible(i)){
-                Tauler aux = new Tauler(t);
-                if(aux.solucio(i, color)){
-                    return i;
-                } else {
-                    // Llamar a minimax, donde yo maximizo y el otro minimza
-                    col = 1;
-                }
+        Integer heuristica = 0;
+        Integer Alpha = Integer.MIN_VALUE; // Alpha es -infinito
+        Integer Beta = Integer.MAX_VALUE; // Beta es +infinito
+
+        int i = 0;
+        boolean solution = false;
+        while(i < t.getMida() && !solution){
+            Tauler aux = new Tauler(t);
+            if(aux.movpossible(i)){ // Miramos si se puede hacer el movimiento
+                aux.afegeix(i, color);
 
             }
+            
+            i++;
         }
+
         return col;
     }
-  
-    public String nom(){
-        return nom;
-    }
+
+   
+
 }
-
-
